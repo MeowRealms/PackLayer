0aa9fba76284d0c53afc6c640c5561fc30c0b6a4
xiaozhangup <xiaozhangshiw@163.com>
2023-12-17 11:17:13 +0800

    清理代码

--- src/main/java/io/th0rgal/packsmanager/velocity/PackLayer.java
+++ src/main/java/io/th0rgal/packsmanager/velocity/PackLayer.java
@@ -4,15 +4,12 @@ import com.google.inject.Inject;
 import com.velocitypowered.api.event.ResultedEvent;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.player.ServerResourcePackSendEvent;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.proxy.protocol.MinecraftPacket;
 import com.velocitypowered.proxy.protocol.packet.RemoveResourcePack;
 import com.velocitypowered.proxy.protocol.packet.ResourcePackRequest;
-import com.velocitypowered.proxy.protocol.packet.ServerData;
 import io.github._4drian3d.vpacketevents.api.event.PacketSendEvent;
 import org.slf4j.Logger;
 
@@ -31,30 +28,21 @@ import java.util.UUID;
         }
 )
 public class PackLayer {
-    private final ProxyServer proxyServer;
-    private final Logger logger;
-    private static PackLayer INSTANCE;
-    private final Map<UUID, String> old = new HashMap<>();
+    private final Map<UUID, String> oldHashes = new HashMap<>();
 
     @Inject
     public PackLayer(ProxyServer proxyServer, Logger logger) {
-        this.proxyServer = proxyServer;
-        this.logger = logger;
+        logger.info("PackLayer Loaded!");
     }
 
     @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-        INSTANCE = this;
-    }
-
-    @Subscribe
-    public void onPacketReceive(PacketSendEvent event) {
+    public void onPacketSend(PacketSendEvent event) {
         final MinecraftPacket packet = event.getPacket();
         final UUID uuid = event.getPlayer().getUniqueId();
         int protocol = event.getPlayer().getProtocolVersion().getProtocol();
 
         if (packet instanceof RemoveResourcePack removeResourcePack) {
-            if (protocol >= 764) {
+            if (protocol >= 764) { // 在 1.20.3+ 的服务端似乎会发一个这玩意, 不取消依然会导致客户端重载
                 event.setResult(ResultedEvent.GenericResult.denied());
             }
         }
@@ -62,17 +50,17 @@ public class PackLayer {
         if (packet instanceof ResourcePackRequest resourcePackRequest) {
             if (protocol == 764) return; // 1.20.2 没救了
 
-            if (old.containsKey(uuid) && old.get(uuid).equals(resourcePackRequest.getHash())) {
+            if (oldHashes.get(uuid).equals(resourcePackRequest.getHash())) {
                 event.setResult(ResultedEvent.GenericResult.denied());
                 return;
             }
-            old.put(uuid, resourcePackRequest.getHash());
+            oldHashes.put(uuid, resourcePackRequest.getHash());
         }
     }
 
     @Subscribe
     public void onDisconnectEvent(DisconnectEvent event) {
         UUID uuid = event.getPlayer().getUniqueId();
-        old.remove(uuid);
+        oldHashes.remove(uuid);
     }
 }
